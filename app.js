80099eb517de6ca4f1582584ad952bf9da1e89d1
Amsax971 <Amsax971>
2024-01-24 22:42:48 +0000

    backend

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express")
 const app = express()
 const port = 8080
 
+const succed = {
+  "code": 200,
+  "Return": "information transfered without trouble"
+}
+
 app.post('/', (req, res) => {
   let informations = req
-  res.send(informations)
+  res.send(succed)
 })
 
 app.listen(port, () => {
