8f4f3e7ee21320429212e2ce3f144c06ca8d828f
Amsax971 <Amsax971>
2024-01-24 19:06:32 +0000

    frontend

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          This is a proxy server, not a website.
+        </h1>
+        <p>this is for educational pruposes only, not any other things</p>
+      </div>
+    </>
+  )
+}
+
+export default App
