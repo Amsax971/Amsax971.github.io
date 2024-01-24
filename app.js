f4ad1096c66f5defa7cd2069d8e37fc4f9584ef0
Amsax971 <Amsax971>
2024-01-24 23:26:19 +0000

    backend

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express")
 const app = express()
 const port = 8080
+const fs = require("fs")
+
+const date = new Date
 
 const succed = {
   "code": 200,
   "Return": "information transfered without trouble"
 }
 
+let name = "{date.hour}{date.day}.json"
+
 app.post('/', (req, res) => {
   let informations = req
+  fs.createWriteStream(name)
+  fs.writeFileSync(name, informations)
   res.send(succed)
 })
 
