703a104c55fa40657c352e384f8b80506945da71
Amsax971 <Amsax971>
2024-01-24 22:29:38 +0000

    backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const app = express()
-const port = 22
+const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/', (req, res) => {
+  let informations = req
+  res.send(informations)
 })
 
 app.listen(port, () => {
